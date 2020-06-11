1ceaa2a99620e49c65c21c99016f058f225bcd9b
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-06-11 17:00:35 -0400

    Update to the latest SDK which adds explicity x86_64 compatibility.

--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -79,7 +79,7 @@ object Deps {
     object Zcash {
         const val ANDROID_WALLET_PLUGINS =   "cash.z.ecc.android:zcash-android-wallet-plugins:1.0.0"
         const val KOTLIN_BIP39 =            "cash.z.ecc.android:kotlin-bip39:1.0.0-beta08"
-        object Sdk :            Version("1.1.0-beta01") {
+        object Sdk :            Version("1.1.0-beta02") {
             val MAINNET =                    "cash.z.ecc.android:sdk-mainnet:$version"
             val TESTNET =                    "cash.z.ecc.android:sdk-testnet:$version"
         }
