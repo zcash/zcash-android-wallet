8e5c87e301ec737a14686e2d450a1fde125f4e3a
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-08-01 02:46:49 -0400

    New: Add biometric authentication support.

--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -13,6 +13,7 @@ object Deps {
     object AndroidX {
         const val ANNOTATION =              "androidx.annotation:annotation:1.1.0"
         const val APPCOMPAT =               "androidx.appcompat:appcompat:1.1.0"
+        const val BIOMETRICS =              "androidx.biometric:biometric:1.1.0-alpha01"
         const val CONSTRAINT_LAYOUT =       "androidx.constraintlayout:constraintlayout:1.1.3"
         const val CORE_KTX =                "androidx.core:core-ktx:1.1.0"
         const val FRAGMENT_KTX =            "androidx.fragment:fragment-ktx:1.1.0-beta01"
