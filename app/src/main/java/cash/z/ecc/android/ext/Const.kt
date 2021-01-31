2e4283ec67cd897c67b9c4ac72da028092441c91
Eljo <elioprifti@gmail.com>
2021-01-31 23:31:43 +0100

    -move buildConfigFiled inside flavour -rename SERVER_URL to DEFAULT_SERVER_URL

--- app/src/main/java/cash/z/ecc/android/ext/Const.kt
+++ app/src/main/java/cash/z/ecc/android/ext/Const.kt
@@ -44,7 +44,7 @@ object Const {
     object Default {
         object Server {
             // If you've forked the ECC repo, change this to your hosted lightwalletd instance
-            const val HOST = BuildConfig.SERVER_URL
+            const val HOST = BuildConfig.DEFAULT_SERVER_URL
             const val PORT = 9067
         }
     }
