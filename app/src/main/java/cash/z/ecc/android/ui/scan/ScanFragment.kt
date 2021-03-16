3bf1ed1415c1fcd422aee9f6e8452b2ec518b8c4
Kevin Gorham <kevin.gorham@electriccoin.co>
2021-03-16 15:08:33 -0400

    Start parsing URIs.
    Initially just do a placeholder implementation for a more full parsing through librustzcash.

--- app/src/main/java/cash/z/ecc/android/ui/scan/ScanFragment.kt
+++ app/src/main/java/cash/z/ecc/android/ui/scan/ScanFragment.kt
@@ -122,13 +122,14 @@ class ScanFragment : BaseFragment<FragmentScanBinding>() {
 
     private fun onQrScanned(qrContent: String, image: ImageProxy) {
         resumedScope.launch {
-            if (viewModel.isNotValid(qrContent)) {
+            val parsed = viewModel.parse(qrContent)
+            if (parsed == null) {
                 val network = ZcashSdk.NETWORK
                 binding.textScanError.text = getString(R.string.scan_invalid_address, network, qrContent)
                 image.close()
             } else {  /* continue scanning*/
                 binding.textScanError.text = ""
-                sendViewModel.toAddress = qrContent
+                sendViewModel.toAddress = parsed
                 mainActivity?.safeNavigate(R.id.action_nav_scan_to_nav_send)
             }
         }
