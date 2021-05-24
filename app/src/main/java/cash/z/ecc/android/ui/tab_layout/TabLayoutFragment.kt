1e5a3b8e8d287a1f5130850ae6d15d211ddfa0af
Kevin Gorham <kevin.gorham@electriccoin.co>
2021-05-24 06:31:14 -0400

    Polish address tabs design and function.
    Restored original styled background.
    Added swipe transition animaiton.
    Let share button remain stationairy during swipe.
    Added transparent icon on t-addr.
    Updated layouts to work on many more devices.
    Fixed fragment caching issue, just let the fragment manager handle it.
    Added share capability.
    Added tap to copy functionality.

--- app/src/main/java/cash/z/ecc/android/ui/tab_layout/TabLayoutFragment.kt
+++ app/src/main/java/cash/z/ecc/android/ui/tab_layout/TabLayoutFragment.kt
@@ -6,20 +6,27 @@ import android.view.View
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
 import cash.z.ecc.android.R
 import cash.z.ecc.android.databinding.FragmentTabLayoutBinding
+import cash.z.ecc.android.di.viewmodel.viewModel
 import cash.z.ecc.android.ext.onClickNavBack
 import cash.z.ecc.android.feedback.Report
 import cash.z.ecc.android.ui.base.BaseFragment
-import cash.z.ecc.android.ui.profile.AwesomeFragment
-import cash.z.ecc.android.ui.receive.ReceiveFragment
+import cash.z.ecc.android.ui.receive.ReceiveTabFragment
+import cash.z.ecc.android.ui.receive.ReceiveViewModel
+import cash.z.ecc.android.ui.receive.TransparentTabFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.launch
 
 class TabLayoutFragment :
     BaseFragment<FragmentTabLayoutBinding>(),
     FragmentCreator,
     TabLayout.OnTabSelectedListener {
+
+    private val viewModel: ReceiveViewModel by viewModel()
 
     override fun inflate(inflater: LayoutInflater): FragmentTabLayoutBinding =
         FragmentTabLayoutBinding.inflate(inflater)
@@ -27,11 +34,26 @@ class TabLayoutFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.hitAreaExit.onClickNavBack { tapped(Report.Tap.RECEIVE_BACK) }
+        binding.textTitle.text = "Receive ${getString(R.string.symbol)}"
         binding.viewPager.adapter = ViewPagerAdapter(this, this)
+        binding.viewPager.setPageTransformer(ZoomOutPageTransformer())
         binding.tabLayout.addOnTabSelectedListener(this)
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = if (position == 0) "Shielded" else "Transparent"
         }.attach()
+        binding.buttonShareAddress.setOnClickListener {
+            shareActiveAddress()
+        }
+    }
+
+    private fun shareActiveAddress() {
+        mainActivity?.apply {
+            lifecycleScope.launch {
+                val address =
+                    if (binding.viewPager.currentItem == 1) viewModel.getTranparentAddress() else viewModel.getAddress()
+                shareText(address)
+            }
+        }
     }
 
     //
@@ -51,16 +73,10 @@ class TabLayoutFragment :
 
     private fun setSelectedTab(@ColorRes color: Int) {
         binding.tabLayout.setSelectedTabIndicatorColor(
-            ContextCompat.getColor(
-                requireContext(),
-                color
-            )
+            ContextCompat.getColor(requireContext(), color)
         )
         binding.tabLayout.setTabTextColors(
-            ContextCompat.getColor(
-                requireContext(),
-                R.color.unselected_tab_grey
-            ),
+            ContextCompat.getColor(requireContext(), R.color.unselected_tab_grey),
             ContextCompat.getColor(requireContext(), color)
         )
     }
@@ -71,11 +87,59 @@ class TabLayoutFragment :
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> ReceiveFragment()
-            1 -> AwesomeFragment()
+            0 -> ReceiveTabFragment()
+            1 -> TransparentTabFragment()
             else -> throw IndexOutOfBoundsException("Cannot create a fragment for index $position")
         }
     }
 
     override fun getItemCount() = 2
+
+    interface AddressFragment {
+        suspend fun getAddress(): String
+    }
+}
+
+private const val MIN_SCALE = 0.8f
+private const val MIN_ALPHA = 0.1f
+
+class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            val pageHeight = height
+            when {
+                position < -1 -> { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    alpha = 0f
+                }
+                position <= 1 -> { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well
+                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horzMargin - vertMargin / 2
+                    } else {
+                        horzMargin + vertMargin / 2
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+
+                    // Fade the page relative to its size.
+                    alpha = (
+                        MIN_ALPHA +
+                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA))
+                        )
+                }
+                else -> { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    alpha = 0f
+                }
+            }
+        }
+    }
 }
